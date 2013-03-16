641c81af9d55f8027db3e73b72a4490acaedba01
David J. Berg <berg.davidj@gmail.com>
2013-03-16 16:04:13 -0700

    write data to CSV for MINE application

---
+++ src/util/writeMINEInput.m
@@ -0,0 +1,23 @@
+function writeMINEInput(path, M, header)
+%M is a variable x observation matrix 
+%header is a cell array with the same number of elements as M has rows
+if (~ismatrix(M))
+    error('first argument must be a matrix of at least two columns');
+end
+
+if (~iscell(header))
+    error('header must be a cell array of vaible names');
+end
+
+if (length(header) ~= size(M, 1))
+    error('The header cell array must have the same number of elements as the input matrix has rows');
+end
+
+ferp = fopen (path, 'w');
+
+for (ii = 1:length(header))
+    fprintf(ferp, '%s ', header{ii});
+end
+fprintf(ferp, '\n');
+
+fprintf(ferp, '%1.6f, %1.6f\n', M);
