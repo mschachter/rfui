e0d546df2a6c8eb3fcf54addff768f4c17cfc263
David J. Berg <berg.davidj@gmail.com>
2013-03-17 23:38:46 -0700

    updated testTuningCurve

--- src/test/testTuningCurve.m
+++ src/test/testTuningCurve.m
@@ -2,7 +2,8 @@ function testTuningCurve(dataFilePath)
 
     edata = ExpData(dataFilePath, 'sig[0-9]*[a-b]*');
     
-    tc1 = TuningCurve(4,5);
+    %order 3 (quadratic), 4 knots (3 spline pieces)
+    tc1 = TuningCurve(3,4);
     tc1.expData = edata;
     tc1.filterWidth = 1;
     tc1.timeOffset = 0;
@@ -10,13 +11,15 @@ function testTuningCurve(dataFilePath)
     tc1.numSubplots = [5, 4];
     tc1.plotTuningCurve();
     
-    %tc3 = TuningCurve(4,3);
-    %tc3.expData = edata;
-    %tc3.selectedCells = [2, 5, 6];
-    %tc3.filterWidth = 1;
-    %tc3.timeOffset = 0;
-    %tc3.useSubplots = 1;
-    %tc3.numSubplots = [4, 5];
-    %tc3.plotTuningCurve();
+    %order 4 (quadratic), as many spline pieces as data points
+    tc3 = TuningCurve(4);
+    tc3.expData = edata;
+    tc3.selectedCells = [2, 5, 6];
+    tc3.filterWidth = 1;
+    tc3.timeOffset = 0;
+    tc3.useSubplots = 1;
+    tc3.numSubplots = [4, 5];
+    tc3.plotTuningCurve();
     
-    %tc3.plotTuningCurve(5);
+    %plot only cell cenumber 5
+    tc3.plotTuningCurve(5);
